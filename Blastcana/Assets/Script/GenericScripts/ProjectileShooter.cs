fb8672154d989af68f45564f9366bc5735b25da6
Chompy <chomp.piro@gmail.com>
2025-04-13 18:01:24 +0200

    Ebauche du script projshooter + implémentation tilemap donjon

--- Blastcana/Assets/Script/GenericScripts/ProjectileShooter.cs
+++ Blastcana/Assets/Script/GenericScripts/ProjectileShooter.cs
@@ -6,25 +6,32 @@ using UnityEngine;
 public class ProjectileShooter : MonoBehaviour
 {
 
+  // Propriétaire du projectile. Référencé pour éviter la collision entre les le tireur et le projectile, son sens d'orientation et pour pouvoir être attribué au projectile.
+  [SerializeField] GameObject Owner;
+  [SerializeField] GameObject ProjectilePrefab;
 
-    [SerializeField] GameObject Owner;
-    [SerializeField] GameObject ProjectilePrefab;
+  //Sens d'orientation du tireur.
+  private int direction;
 
-    private float direction;
 
-    public void OnFire(UnityEngine.InputSystem.InputAction.CallbackContext context)
-    {
-      /*  if(Owner.TryGetComponent(out CharacterAttack characterscript)){
-        direction = characterscript.direction;
-        }
-        else if(Owner.TryGetComponent(out MonsterRange monsterscript)){
-        direction = monsterscript.direction;
-        }
-        */
-        var proj = Instantiate(ProjectilePrefab, transform.position, Quaternion.identity);
-        /*if(proj.TryGetComponent(out Projectile projectilescript)){
-          projectilescript.owner = Owner;
-        }
-        */
+  // Fonction a appeler pour tirer un projectile.
+  public void OnFire()
+  {
+    // Cherche à obtenir la référence du script du propriétaire pour obtenir son sens d'orientation.
+    /*  if(Owner.TryGetComponent(out CharacterAttack characterscript)){
+      direction = characterscript.direction;
+      }
+      else if(Owner.TryGetComponent(out MonsterRange monsterscript)){
+      direction = monsterscript.direction;
+      }
+      */
+    // Instancie le projectile en devant le tireur en fonction de son orientation.
+    var proj = Instantiate(ProjectilePrefab, new Vector2(transform.position.x, transform.position.y) + Vector2.right * 10 * direction, Quaternion.identity);
+
+    // Donne la référence du tireur au projectile instancié.
+    /*if(proj.TryGetComponent(out Projectile projectilescript)){
+      projectilescript.owner = Owner;
     }
+    */
+  }
 }
